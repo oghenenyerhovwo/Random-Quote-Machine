7de861539e605a916b8453faebe07d1e1441ed6e
oghenenyerhovwo <93879454+oghenenyerhovwo@users.noreply.github.com>
2022-01-20 20:34:01 +0100

    Set up background color

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+import colors from "./data/color";
 
 
 function App() {
+  const [color, setColor] = useState("white")
+  useEffect(() => {
+    const n = Math. floor(Math. random() * 15)
+    setColor(colors[n])
+  }, [])
+
   return (
-    <div className="App">
+    <div
+      style ={{
+        display: "inline-block",
+        height: "100vh",
+        width: "100%",
+        background: color,
+      }}
+    >
       Good
     </div>
   );
