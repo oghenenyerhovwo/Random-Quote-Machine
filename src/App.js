3d85ccecad4c04cabb9f01e3321d76fa4c14a588
oghenenyerhovwo <93879454+oghenenyerhovwo@users.noreply.github.com>
2022-01-21 21:02:12 +0100

    Connected to the quote api and rendering

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
+import axios from 'axios';
 import { useEffect, useState } from "react";
 import colors from "./data/color";
 
 
 function App() {
   const [color, setColor] = useState("#333")
-  useEffect(() => {
-    const n = Math.floor(Math. random() * 15)
+  const [changeColor, setChangeColor] = useState(false)
+  const [quotes, setQuotes] = useState([])
+  const [quote, setQuote] = useState({})
+
+const setNewColor = () => {
+    const n = Math.floor(Math.random() * 15)
     setColor(colors[n])
+  }
+
+const setQoutesArray = () => {
+  axios
+      .get(
+          "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",
+        )
+      .then(res =>{ 
+        setQuotes(res.data.quotes)
+        setNewColor()
+      })
+      .catch(err => console.error(err));
+}
+
+  useEffect(() => {
+    setQoutesArray() 
   }, [])
+
+  
+
+  const getQuote = () => {
+    if(quotes.length > 1){
+      const q_index = Math.floor(Math.random() * quotes.length)
+      setQuote(quotes[q_index])
+    } else {
+      setQoutesArray()
+    }
+  }
+
+  const handleClick = () => {
+    getQuote()
+    setChangeColor(true)
+    if (changeColor){
+      setNewColor()
+    }
+  }
 
   return (
     <div
@@ -32,12 +72,15 @@ function App() {
           zIndex: 2,
         }}
       >
-        <div>
+        <div style={{marginBottom: "90px"}}>
           <h1 style={{color: color, fontSize: "3rem"}} >
-            <i class="fa fa-quote-left" aria-hidden="true"></i>
+            <i className="fa fa-quote-left" aria-hidden="true"></i>
             {" "}
-
+            {quotes && quote.quote}
           </h1>
+          <div>
+            <div style={{display: "inline-block",color: color, float: "right", fontSize: "1.2rem"}}>-{quotes && quote.author}</div>
+          </div>
         </div> 
         <div>
           <div
@@ -46,9 +89,9 @@ function App() {
             }}
           >
             <button
-              style={{borderRadius: "3px", fontSize: "0.9rem", background: color, border: "none", padding: "10px", color: "#fff" }}
+              style={{borderRadius: "3px", fontSize: "1.2rem", background: color, border: "none", padding: "10px", color: "#fff" }}
             >
-              <i class="fa fa-twitter" aria-hidden="true"></i>
+              <i className="fa fa-twitter" aria-hidden="true"></i>
             </button>
           </div>
           <div
@@ -58,6 +101,7 @@ function App() {
             }}
           >
             <button
+              onClick={handleClick}
               style={{borderRadius: "3px", background: color, border: "none", padding: "10px", color: "#fff" }}
             >
               New Quote
